7f26d24aa12c1c0e1e39d1f301fc3a4f4f5c76fc
LaDev-thePastoralDev <langatwilliamk@gmail.com>
2018-10-05 17:31:12 +0300

    Build CLI

--- index.js
+++ index.js
@@ -2,31 +2,32 @@
  * Primary file for the API
  * 
  */
-//Dependencies
-const server = require('./lib/server');
-const workers = require('./lib/workers');
-const helpers = require('./lib/helpers');
-const cli = require('./lib/cli');
 
-helpers.sendTwilioSms('4158375309', `Hello`, function(err) {
-    console.log(`Error: ${err}`);
+//Dependencies
+const server = require("./lib/server");
+const workers = require("./lib/workers");
+const helpers = require("./lib/helpers");
+const cli = require("./lib/cli");
+
+helpers.sendTwilioSms("4158375309", `Hello`, function(err) {
+  console.log(`Error: ${err}`);
 });
 
 // Declare the app
 const app = {};
 
-// Init the function 
+// Init the function
 app.init = function() {
-    // Start the server
-    server.init();
+  // Start the server
+  server.init();
 
-    // Start the workers
-    workers.init();
+  // Start the workers
+  workers.init();
 
-    // Start the CLI, but we make sure it starts last
-    setTimeout(function(){
-        cli.init();
-    }, 50);
+  // Start the CLI, but we make sure it starts last
+  setTimeout(function() {
+    cli.init();
+  }, 50);
 };
 
 // Execute
